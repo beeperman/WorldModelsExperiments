514e87e9562eff12530761e82ba48bf1b0a75212
Fangda Gu <beeperman@live.com>
2019-06-01 11:18:07 +0800

    edited test scripts

--- objects/train_controller.bash
+++ objects/train_controller.bash
@@ -1,27 +1,29 @@
-beta="
-1"
+beta="1
+"
 
 #0: old wall avoiding agent
 #1: new wall avoiding agent
 #2: no kl tolerence
 
-var=4
+var=7
+#b=100
+#CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --loadrnn --beta $b --int 4 &> train_bash_log/controller_b${b}_4_rnn &
 for b in $beta
 do
-  #name=${f:12:-5}
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 20 &> train_bash_log/controller_b${b}_20_all &
+  name=${f:12:-5}
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --loadrnn --beta $b --int 20 &> train_bash_log/controller_b${b}_20_rnn &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 21 &> train_bash_log/controller_b${b}_21_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --loadrnn --beta $b --int 21 &> train_bash_log/controller_b${b}_21_rnn &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 22 &> train_bash_log/controller_b${b}_22_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --loadrnn --beta $b --int 22 &> train_bash_log/controller_b${b}_22_rnn &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 23 &> train_bash_log/controller_b${b}_23_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --loadrnn --beta $b --int 23 &> train_bash_log/controller_b${b}_23_rnn &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
- # CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 24 &> train_bash_log/controller_b${b}_24_all &
- # echo "${var}th name: $0 assigned to $(($var%8))th gpu"
- # var=$((var+1))
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 4 &> train_bash_log/controller_b${b}_4_no &
+  echo "${var}th name: $0 assigned to $(($var%8))th gpu"
+  var=$((var+1))
 done
--- objects/train_memory.bash
+++ objects/train_memory.bash
@@ -1,24 +1,25 @@
 beta="
-1"
+200"
 
 #0: old wall avoiding agent
 #1: new wall avoiding agent
 #2: no kl tolerence
 
 var=0
+#CUDA_VISIBLE_DEVICES=2 python train_memory.py --beta 100 --int 4 &> train_bash_log/rnn_b100_4_all &
 for b in $beta
 do
   #name=${f:12:-5}
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 20 &> train_bash_log/rnn_b${b}_20_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 10 &> train_bash_log/rnn_b${b}_10_all &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 21 &> train_bash_log/rnn_b${b}_21_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 11 &> train_bash_log/rnn_b${b}_11_all &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 22 &> train_bash_log/rnn_b${b}_22_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 12 &> train_bash_log/rnn_b${b}_12_all &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 23 &> train_bash_log/rnn_b${b}_23_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 13 &> train_bash_log/rnn_b${b}_13_all &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
  # CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 24 &> train_bash_log/rnn_b${b}_24_all &
