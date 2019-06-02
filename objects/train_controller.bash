b0979373379fd74a294cfcd7067a982f4f653ba1
Fangda Gu <beeperman@live.com>
2019-06-02 08:44:00 +0800

    pre merge

--- objects/train_controller.bash
+++ objects/train_controller.bash
@@ -1,29 +1,29 @@
-beta="50
+beta="1
 "
 
 #0: old wall avoiding agent
 #1: new wall avoiding agent
 #2: no kl tolerence
 
-var=1
+var=0
 #b=100
 #CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --loadrnn --beta $b --int 4 &> train_bash_log/controller_b${b}_4_rnn &
 for b in $beta
 do
   name=${f:12:-5}
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 0 &> train_bash_log/controller_b${b}_0_nornn &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 20 --seed 1 &> train_bash_log/controller_b${b}_20_nornn_s1 &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 1 &> train_bash_log/controller_b${b}_1_nornn &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 21 --seed 1 &> train_bash_log/controller_b${b}_21_nornn_s1 &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 2 &> train_bash_log/controller_b${b}_2_nornn &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 22 --seed 1 &> train_bash_log/controller_b${b}_22_nornn_s1 &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 3 &> train_bash_log/controller_b${b}_3_nornn &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 23 --seed 1 &> train_bash_log/controller_b${b}_23_nornn_s1 &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 4 &> train_bash_log/controller_b${b}_4_nornn &
-  echo "${var}th name: $0 assigned to $(($var%8))th gpu"
-  var=$((var+1))
+  #CUDA_VISIBLE_DEVICES=$(($var%8)) python train_controller.py --beta $b --int 4 &> train_bash_log/controller_b${b}_4_nornn &
+  #echo "${var}th name: $0 assigned to $(($var%8))th gpu"
+  #var=$((var+1))
 done
--- objects/train_memory.bash
+++ objects/train_memory.bash
@@ -1,25 +1,25 @@
 beta="
-200"
+1"
 
 #0: old wall avoiding agent
 #1: new wall avoiding agent
 #2: no kl tolerence
 
-var=0
+var=4
 #CUDA_VISIBLE_DEVICES=2 python train_memory.py --beta 100 --int 4 &> train_bash_log/rnn_b100_4_all &
 for b in $beta
 do
   #name=${f:12:-5}
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 10 &> train_bash_log/rnn_b${b}_10_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 20 --datadir train_record/stagett --name tt &> train_bash_log/rnn_b${b}_20_tt &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 11 &> train_bash_log/rnn_b${b}_11_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 21 --datadir train_record/stagett --name tt &> train_bash_log/rnn_b${b}_21_tt &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 12 &> train_bash_log/rnn_b${b}_12_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 22 --datadir train_record/stagett --name tt &> train_bash_log/rnn_b${b}_22_tt &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
-  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 13 &> train_bash_log/rnn_b${b}_13_all &
+  CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 23 --datadir train_record/stagett --name tt &> train_bash_log/rnn_b${b}_23_tt &
   echo "${var}th name: $0 assigned to $(($var%8))th gpu"
   var=$((var+1))
  # CUDA_VISIBLE_DEVICES=$(($var%8)) python train_memory.py --beta $b --int 24 &> train_bash_log/rnn_b${b}_24_all &
